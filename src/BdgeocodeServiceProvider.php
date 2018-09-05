adee32be2188ed3c67d18e8f8bc6347692caa65f
Sand Of Vega <sandofvega@gmail.com>
2018-09-05 21:25:47 +0600

    service provider ready to use

---
+++ src/BdgeocodeServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sandofvega\Bdgeocode;
+
+use Illuminate\Support\ServiceProvider;
+
+class BdgeocodeServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
