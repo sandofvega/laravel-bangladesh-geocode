7d6b22a0357b59488547cdeb71afadb97b0ee441
Sand Of Vega <sandofvega@gmail.com>
2018-09-07 09:58:49 +0600

    add sql

---
+++ src/data/sql/divisions.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 07, 2018 at 03:52 AM
+-- Server version: 10.0.36-MariaDB-0ubuntu0.16.04.1
+-- PHP Version: 7.2.9-1+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bdgeocode`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `divisions`
+--
+
+CREATE TABLE `divisions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bn_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `divisions`
+--
+
+INSERT INTO `divisions` (`id`, `name`, `bn_name`) VALUES
+(1, 'Barishal', 'বরিশাল'),
+(2, 'Chattogram', 'চট্টগ্রাম'),
+(3, 'Dhaka', 'ঢাকা'),
+(4, 'Khulna', 'খুলনা'),
+(5, 'Rajshahi', 'রাজশাহী'),
+(6, 'Rangpur', 'রংপুর'),
+(7, 'Sylhet', 'সিলেট'),
+(8, 'Mymensingh', 'ময়মনসিংহ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `divisions`
+--
+ALTER TABLE `divisions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `divisions_name_unique` (`name`),
+  ADD UNIQUE KEY `divisions_bn_name_unique` (`bn_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `divisions`
+--
+ALTER TABLE `divisions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/seeds/DivisionsTableSeeder.php
+++ src/seeds/DivisionsTableSeeder.php
@@ -9,7 +9,7 @@ class DivisionsTableSeeder extends Seeder
 {
     public function run()
     {
-        $divisions = json_decode(file_get_contents(__DIR__.'/../data/divisions.json'), true);
+        $divisions = json_decode(file_get_contents(__DIR__.'/../data/json/divisions.json'), true);
         DB::table('divisions')->insert($divisions);
     }
 }
