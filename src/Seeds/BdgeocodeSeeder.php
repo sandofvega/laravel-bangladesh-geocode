b86ebf4a5c29170c9f40c82aa503b38998ef39e0
Sand Of Vega <sandofvega@gmail.com>
2018-09-10 11:10:49 +0600

    add transaction to bdgeocodeseeder.php

--- src/Seeds/BdgeocodeSeeder.php
+++ src/Seeds/BdgeocodeSeeder.php
@@ -3,14 +3,19 @@
 namespace Sandofvega\Bdgeocode\Seeds;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BdgeocodeSeeder extends Seeder
 {
     public function run()
     {
+        DB::beginTransaction();
+
         $this->call(DivisionsTableSeeder::class);
         $this->call(DistrictsTableSeeder::class);
         $this->call(ThanasTableSeeder::class);
         $this->call(UnionsTableSeeder::class);
+
+        DB::commit();
     }
 }
