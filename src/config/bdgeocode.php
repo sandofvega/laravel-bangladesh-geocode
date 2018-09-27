af0f087e90e30e44bafb0bc6b8b9731ed244b939
Sand Of Vega <sandofvega@gmail.com>
2018-09-27 15:12:35 +0600

    model published

---
+++ src/config/bdgeocode.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of BdGeoCode,
+ * 8 Division, 64 District, 491 Thana and 2350 Union fully mapped laravel package.
+ *
+ * @license MIT
+ * @package sandofvega/bdgeocode
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Models path
+    |--------------------------------------------------------------------------
+    |
+    | Update the model_namespace if your Models is in a different folder.
+    |
+    */
+    'models_path' => app_path('')
+];
